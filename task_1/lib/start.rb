7009f22f1b44f8f52121c893a1f87b4eafc752fd
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2024-09-22 07:20:37 +0300

    added console output

--- task_1/lib/start.rb
+++ task_1/lib/start.rb
@@ -5,3 +5,12 @@ output_file_path = '../data/sorted_transactions.txt'
 File.delete(output_file_path) if File.exist?(output_file_path)
 Algorithm::TransactionSorter.sort_transactions(file_path, output_file_path)
 
+puts "\t\ntransactions:\n\n"
+File.foreach(file_path) do |line|
+  puts line
+end
+
+puts "\t\nsorted_transactions:\n\n"
+File.foreach(output_file_path) do |line|
+  puts line
+end
